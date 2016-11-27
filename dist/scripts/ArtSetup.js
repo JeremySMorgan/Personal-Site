1abef0934154dbf74b1847cc471a7fe4fc4c22c7
admin <jsmorgan6@gmail.com>
2016-11-27 18:17:20 -0500

    Remove unneccessary code and restructure project

--- dist/scripts/ArtSetup.js
+++ dist/scripts/ArtSetup.js
@@ -2,62 +2,39 @@
 
 $(document).ready(function() {
 
-
-  //xOffset = Number( $('#page-content-wrapper').css('padding-left').replace(/\D/g,'') )* 20;
-
-  window.addEventListener("resize", function(){
-      console.log("resize!");
-      screen_width = window.innerWidth;
-      screen_height = window.innerHeight;
-      //setupCanvas( screen_width - xOffset, screen_height );
+  window.addEventListener("resize", function() {
+    console.log("resize!");
+    screen_width = window.innerWidth;
+    screen_height = window.innerHeight;
   });
-
 
   screen_width = window.innerWidth;
   screen_height = window.innerHeight;
 
-  xOffset = Number($('#page-content-wrapper').css('padding-left').replace(/\D/g,'')) * 2 ;
+  xOffset = Number($('#page-content-wrapper').css('padding-left').replace(/\D/g, '')) * 2;
 
-  console.log(typeof xOffset);
-  console.log( (xOffset) );
-
-
-  setupCanvas( screen_width - xOffset, screen_height-70 );
-  //setupCanvas( screen_width -20 , screen_height -70);
-
+  setupCanvas(screen_width - xOffset, screen_height - 70);
 });
 
-
-
-function setupCanvas(screen_width,screen_height){
-
+function setupCanvas(screen_width, screen_height) {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
-
-  ctx.canvas.width  = screen_width;
+  ctx.canvas.width = screen_width;
   ctx.canvas.height = screen_height;
-
-  runtime(screen_width,screen_height);
-
+  runtime(screen_width, screen_height);
 }
-
-
-
-
-
-
 
 // ------------------- // Sliders // ------------------- //
 
-var sliderIds = ["pathTimeDelay","timeSmallCircleDelay","radius","largeRadius","xToCirclePathConstant","yTocirclePathConstant","xFromCirclePathConstant","yFromcirclePathConstant"];
+var sliderIds = ["pathTimeDelay", "timeSmallCircleDelay", "radius", "largeRadius", "xToCirclePathConstant", "yTocirclePathConstant", "xFromCirclePathConstant", "yFromcirclePathConstant"];
 
-for (var i =0; i<sliderIds.length; i++){
+for (var i = 0; i < sliderIds.length; i++) {
   noUiSlider.create(document.getElementById(sliderIds[i]), {
-  	start: [50],
-  	connect: false,
-  	range: {
-  		'min': 0,
-  		'max': 100
-  	}
+    start: [50],
+    connect: false,
+    range: {
+      'min': 0,
+      'max': 100
+    }
   });
 }
