11f480e7a46efc212183c8d754f1a590ad26e13e
admin <jsmorgan6@gmail.com>
2016-11-27 19:14:19 -0500

    Updated Projects, Contact

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -17,33 +17,33 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
+  .config(function($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         //controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/projects', {
-        templateUrl: 'views/projects.html',
-        //controller: 'ProjectsCtrl',
-        controllerAs: 'projects'
-      })
-      .when('/art', {
-        templateUrl: 'views/art.html',
-        //controller: 'ArtCtrl',
-        controllerAs: 'art'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        //controller: 'ContactCtrl',
-        controllerAs: 'contact'
-      })
-  /*    .when('/media', {
-        templateUrl: 'views/media.html',
-        //controller: 'MediaCtrl',
-        controllerAs: 'media'
-      })*/
+            .when('/projects', {
+              templateUrl: 'views/projects.html',
+              //controller: 'ProjectsCtrl',
+              controllerAs: 'projects'
+            })
+            .when('/art', {
+              templateUrl: 'views/art.html',
+              //controller: 'ArtCtrl',
+              controllerAs: 'art'
+            })
+            .when('/contact', {
+              templateUrl: 'views/contact.html',
+              //controller: 'ContactCtrl',
+              controllerAs: 'contact'
+            })
+        /*    .when('/media', {
+              templateUrl: 'views/media.html',
+              //controller: 'MediaCtrl',
+              controllerAs: 'media'
+            })*/
       .otherwise({
         redirectTo: '/'
       });
